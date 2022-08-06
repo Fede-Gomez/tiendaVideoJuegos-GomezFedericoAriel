aa5ed8dc5db50458b977fde69c7a97519e2db27b
Federico Ariel Gomez <feago@hotmail.com>
2022-08-06 19:06:30 -0300

    menu (sin bootstrap, mas adelante se cambiara por una libreria)

---
+++ src/components/NavBar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import '../styles/NavBar.css'
+import logo from '../assets/logo/logo.jpg'
+
+
+export const NavBar = () => {
+  return (
+    <>
+        <nav className='navBar'>
+            <ul>
+                <li className='img'>
+                    <img src={logo} />
+                </li>
+                <li>
+                    <button className='btn-inicio'>Inicio</button>
+                </li>
+                <li className='categoria'>
+                    <button className='btn-categoria'>Videojuegos</button>
+                </li>
+                <li className='categoria'>
+                    <button className='btn-categoria'>Computadoras</button>
+                </li>
+                <li className='categoria'>
+                    <button className='btn-categoria'>Notebooks</button>
+                </li>
+                <li className='categoria'>
+                    <button className='btn-categoria'>Merchandising</button>
+                </li>
+                <li className='login'>
+                    <button className='btn-login'>Login</button>
+                </li>
+            </ul>
+        </nav>
+    </>
+  )
+}
